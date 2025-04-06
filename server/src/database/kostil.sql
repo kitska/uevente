a2974124ba1083f61e43c6cd3bca3bc5e14b0f9a
myth cat <xdxddd932@gmail.com>
2025-04-06 13:43:44 +0300

    -server

--- server/src/database/kostil.sql
+++
@@ -1,7 +0,0 @@
-CREATE TYPE rights_enum AS ENUM ('owner', 'editor', 'viewer');
-
-ALTER TABLE calendar_users
-ADD COLUMN rights rights_enum NOT NULL DEFAULT 'viewer';
-
-ALTER TABLE event_users
-ADD COLUMN rights rights_enum NOT NULL DEFAULT 'viewer';
--- server/types/express.d.ts
+++
@@ -1,1 +0,0 @@
-// types/express.d.ts
